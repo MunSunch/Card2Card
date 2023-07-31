491340f8c35d806f4e3d9580daddba798ca3a2d0
MunirTheBest <msunchalyaev@gmail.com>
2023-07-31 18:55:51 +0300

    init commit

---
+++ src/main/java/com/munsun/application/Card2CardProject/Card2CardProjectApplication.java
@@ -0,0 +1,13 @@
+package com.munsun.application.Card2CardProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Card2CardProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Card2CardProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/munsun/application/Card2CardProject/Card2CardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.munsun.application.Card2CardProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Card2CardProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
